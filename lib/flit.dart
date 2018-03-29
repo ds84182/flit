6c4d950756095ef887c8b0226761827a3199d91b
Dwayne Slater <ds84182@gmail.com>
2018-03-29 18:40:49 -0400

    Use a wrapper widget instead of PlatformOverlay

--- lib/flit.dart
+++ lib/flit.dart
@@ -8,12 +8,33 @@ import 'src/sdl_text_input.dart';
 
 void flitInit() {
   if (Platform.isWindows) {
-    PlatformOverlay.addOverlay((context, child) {
-      return new TitleBarOverlay(child: child);
-    });
-
     initTitleBarChannel();
   }
 
   initSDLTextInput();
+}
+
+/// Widget that wraps an app in platform-specific widgets.
+///
+/// Usage:
+///
+/// Add (or merge into, if it exists) this line to your MaterialApp.
+///
+/// ```dart
+/// builder: (context, child) => new FlitAppWrapper(child: child),
+/// ```
+class FlitAppWrapper extends StatelessWidget {
+  final Widget child;
+  const FlitAppWrapper({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isWindows) {
+      return new TitleBarOverlay(
+        child: child,
+      );
+    } else {
+      return child;
+    }
+  }
 }
